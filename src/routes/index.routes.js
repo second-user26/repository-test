26b22d87496a739aa5e7889f39e38014d254f876
second-user26 <sarmientochris239@gmail.com>
2026-09-18 19:03:42 +0000

    B-1: Add teacher routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const userRoutes = require('./user.routes');
 const authRoutes = require('./auth.routes');
+const teacherRoutes = require('./teacher.routes');
 
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use('/auth', authRoutes);
+router.use('/teacher', teacherRoutes);
 
 
 module.exports = router;
