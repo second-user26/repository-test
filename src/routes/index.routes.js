e791f9ff47057a0060c870287e63743e3ce8598d
second-user26 <sarmientochris239@gmail.com>
2026-09-18 18:57:44 +0000

    add index routes

---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
+
+const router = express.Router();
+
+router.use('/user', userRoutes);
+router.use('/auth', authRoutes);
+
+
+module.exports = router;
